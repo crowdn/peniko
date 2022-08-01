4ee0a57ecd29f7429cdaa620aaf7531750903b5c
pengguangyou <pengguangyou@kuaishou.com>
2022-08-01 20:23:32 +0800

    update-Mon Aug 01 2022

--- publish.sh
+++ publish.sh
@@ -2,7 +2,7 @@ if [ ! -d "./dist" ]; then
   mkdir dist
 fi
 npm version patch 
-cp ./package.json ./dist/package.json
+cp ./peniko.pkg.json ./dist/package.json
 cp ./README.md ./dist/README.md
 
 npm run build
